ec508a3af808b16efbb2024f70425ea9e89691e6
Cbinbin <1142293069@qq.com>
2017-02-12 18:07:04 +0800

    developer and customer add in user

--- models/User.js
+++ models/User.js
@@ -1,11 +1,50 @@
 const mongoose = require('mongoose')
 	, Schema = mongoose.Schema
+	, Project = require('./Project')
 
 const userSchema = new Schema({
+	//普通用户字段
 	openid: { type: String },
 	nickname: { type: String },
 	sex: { type: String },
 	headimg: { type: String },
-	origin: { type: String }
+	origin: { type: String },
+	//开发者字段
+	position: { type: String },
+	QQ: { type: Number },
+	signature: { type: String },
+	introduction: { type: String },
+	status: { 
+		type: String, 
+		enum: ['on', 'off'] 
+	},
+	projectTime: { type: Number },
+	totalTime: { type: Number },
+	doing: { 
+		type: Schema.Types.ObjectId, 
+		ref:'Project' 
+	},
+	participations: [{ 
+		type: Schema.Types.ObjectId, 
+		ref:'Project' 
+	}],
+	//客户字段
+	realname: { type: String },
+	telephone: { type: Number },
+	company: { type: String },
+	companyLogo: { type: String },
+	companyAddress: { type: String },
+	fax: { type: String },
+	seniority: { type: Number },
+	serviceDate: { type: String },
+	onGoing: [{ 
+		type: Schema.Types.ObjectId, 
+		ref:'Project' 
+	}],
+	finishProjects: [{ 
+		type: Schema.Types.ObjectId, 
+		ref:'Project' 
+	}],
+	remark: { type: String }
 })
 module.exports = mongoose.model('User', userSchema)
