91c19d23478065d4e7a527d1b087e732f2286314
binbin Chen <cbinbin@binbindeMacBook-Air.local>
2017-02-22 11:15:57 +0800

    change

--- routes/home.js
+++ routes/home.js
@@ -4,10 +4,19 @@ const router = require('express').Router()
 router.get('/', (req, res)=> {
 	res.json({
 		api: host.bridge,
-		admin: host.bridge + 'admin',
-		user: host.bridge + 'user',
+		login: host.bridge + 'login',
+		admin_user: host.bridge + 'admin/user?token=${token}',
+		admin_customer: host.bridge + 'admin/customer?token=${token}',
+		admin_developer: host.bridge + 'admin/developer?token=${token}',
+		admin_projects: host.bridge + 'admin/project?token=${token}',
+		admin_project: host.bridge + 'admin/project/:id?token=${token}',
 		session: host.bridge + 'session',
-		project: host.bridge + 'project'
+		projects: host.bridge + 'project',
+		project: host.bridge + 'project/:id?token=${token}',
+		schedule: host.bridge + 'project/:id/schedule?token=${token}',
+		design: host.bridge + 'project/:id/design?token=${token}',
+		document: host.bridge + 'project/:id/document?token=${token}',
+		user: host.bridge + 'user?token=${token}',
 	})
 })
 
